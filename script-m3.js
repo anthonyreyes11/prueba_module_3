384060a02e3f425555bd7c311201ff5cb39937b7
anthony <anthonyreyes11@gmail.com>
2022-06-28 14:41:42 -0400

    boton reset 100% funcionando, boton hard funciona 50%

--- script-m3.js
+++ script-m3.js
@@ -4,16 +4,18 @@ const span = document.querySelector("#colorDisplay");
 const resetButton = document.querySelector("#reset");
 const easyBtn = document.querySelector("#easyBtn");
 const hardBtn = document.querySelector("#hardBtn");
+const messageDisplay = document.querySelector(".message");
+const numSquares = 6;
 
 
 
 
 // VARIABLES
 //let colores = ["rgb(240, 14, 128)", "rgb(40, 164, 12)", "rgb(20, 114, 158)", "rgb(10, 14, 12)", "rgb(2, 194, 128)", "rgb(240, 149, 128)"];
-const colors = generateRandomColors(6);
+const colors = generateRandomColors(numSquares);
 
-  
-function changeColors(colors){
+
+function changeColors(colors) {
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.background = colors;
   };
@@ -37,18 +39,39 @@ function generateRandomColors(num) {
 for (let i = 0; i < colors.length; i++) {
   squares[i].style.backgroundColor = colors[i];
 
-  squares[i].addEventListener("click", function() {
+  squares[i].addEventListener("click", function () {
     let clickedColor = this.style['background-color'];
-    
+
     if (clickedColor === pickedColor) {
 
       changeColors(clickedColor);
-      span.style.background = clickedColor;
-    }else{
+      span.style.backgroundColor = clickedColor;
+      messageDisplay.textContent = "Eres Seco!";
+    } else {
       this.style.background = "#232323";
+      messageDisplay.textContent = "Intentalo de nuevo!";
     }
   })
 };
+
+resetButton.addEventListener("click", function () {
+  location.reload();
+});
+
+// boton hard === boton reset
+hardBtn.addEventListener("click", function(){
+  console.log('funcionando');
+  messageDisplay.textContent = "";
+  easyBtn.classList.remove('#easyBtn');
+  hardBtn.classList.add('elegido');
+
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+    squares[i].style.display = "block";
+}
+
+});
+
 
 function randomColor() {
   const r = Math.floor(Math.random() * 256)
@@ -58,6 +81,6 @@ function randomColor() {
 };
 
 span.textContent = pickedColor;
-span.style.color = pickedColor;
+
 
 

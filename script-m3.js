a13ee4920856a200b38e5c58d1e23d53c3729fe4
anthony <anthonyreyes11@gmail.com>
2022-06-28 10:44:35 -0400

    se genera el cambio de colores para los squares y el pickedcolors

--- script-m3.js
+++ script-m3.js
@@ -1,31 +1,30 @@
 // ETIQUETAS HTML
 const squares = document.querySelectorAll(".square");
 const h1 = document.querySelector("#colorDisplay");
-const rgbCode = document.querySelector("#rgbCode");
-const resetButton = document.querySelector("#reset");
-const easyBtn = document.querySelector("#easyBtn");
-const hardBtn = document.querySelector("#hardBtn");
+
+
 
 
 
 // VARIABLES
-let colores = ["rgb(240, 14, 128)", "rgb(40, 164, 12)", "rgb(20, 114, 158)", "rgb(10, 14, 12)", "rgb(2, 194, 128)", "rgb(240, 149, 128)"];
+//let colores = ["rgb(240, 14, 128)", "rgb(40, 164, 12)", "rgb(20, 114, 158)", "rgb(10, 14, 12)", "rgb(2, 194, 128)", "rgb(240, 149, 128)"];
+const colores = generateRandomColors(6);
 
-for(let i = 0; i < colores.length ; i++){
-  squares[i].style.backgroundColor = colores [i];
+for (let i = 0; i < colores.length; i++) {
+  squares[i].style.backgroundColor = colores[i];
 }
 
 
 // funciones
 function pickColor() {
   return colores[Math.floor(colores.length * Math.random())]
-}
+};
 const pickedColor = pickColor();
 
 
 function generateRandomColors(num) {
   const arr = []
-  for (var i = 0; i < num; i++) {
+  for (i = 0; i < num; i++) {
     arr.push(randomColor())
   }
   return arr;
@@ -38,29 +37,9 @@ function randomColor() {
   return "rgb(" + r + ", " + g + ", " + b + ")";
 }
 
-easyBtn.addEventListener("click", function () {
-  console.log('funcionando')
-  hardBtn.classList.remove("elegido");
-  easyBtn.classList.add("elegido");
-
-  for (i = 0; i < squares.length; i++) {
-    if (colores[i]) {
-      squares[i].style.background = colores[i];
-    } else {
-      squares[i].style.display = "none";
-    }
-  }
-});
-
-rgbCode.textContent = pickedColor;
+h1.textContent = pickedColor;
+h1.style.color = pickedColor;
 
 
-  hardBtn.addEventListener("click", function () {
-    console.log('funcionando')
-  });
-
-reset.addEventListener("click", function () {
-  console.log('funcionando')
-});
 
 
